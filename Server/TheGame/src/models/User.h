bc0cf49b617fe1cf07c1f0fb2f2aa94d94849c72
Denisucu1 <denisro463@gmail.com>
2025-10-29 00:24:14 +0200

    Update User.h

---
+++ Server/TheGame/src/models/User.h
@@ -0,0 +1,17 @@
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+
+struct User {
+
+    int id = 0;
+    std::string username;
+    int games_played = 0;
+    int games_won = 0;
+    float total_cards_at_loss = 0.0f;
+    int total_time_minutes = 0;
+    int performance_score = 1;
+};
+
+#endif

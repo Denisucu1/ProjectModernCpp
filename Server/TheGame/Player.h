54604615bd9e57cc8a9ea244f133f3c8a705c9eb
aurelianv1 <auras.anca17@gmail.com>
2025-11-15 13:49:30 +0200

    Add score tracking to Player class
    Introduced m_score member to Player, initialized in the constructor. Added SetScore and GetScore methods to manage and access the player's score.

--- Server/TheGame/Player.h
+++ Server/TheGame/Player.h
@@ -10,8 +10,13 @@ public:
 	void SetDeck(std::vector<std::uint8_t> deck);
 
 	std::vector<std::uint8_t> GetDeck() const;
+
+	void SetScore(std::uint8_t score);
+
+	std::uint8_t GetScore() const;
 private:
 	std::string m_name;
 	std::vector<std::uint8_t> m_deck;
+	std::uint8_t m_score;
 };
 

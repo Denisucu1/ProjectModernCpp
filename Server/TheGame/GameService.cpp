e3241d811031d239f9cf6060e37519d8950dbdc6
aurelianv1 <auras.anca17@gmail.com>
2025-11-15 15:57:54 +0200

    Matchmaking
    I added a matchmaking for the game

--- Server/TheGame/GameService.cpp
+++ Server/TheGame/GameService.cpp
@@ -1,1 +1,24 @@
 #include "GameService.h"
+
+GameService::GameService()
+{
+}
+
+std::optional<GameId> GameService::FindGame(UserId userId, const std::string& username, int desiredPlayerCount)
+{
+    //
+}
+
+std::optional<GameId> GameService::GetPlayerGameStatus(UserId userId)
+{
+    //
+}
+
+Game& GameService::GetGame(const GameId gameId)
+{
+    // TODO: insert return statement here
+}
+
+void GameService::CreateGame(std::list<WaitingPlayer>& players, int playerCount)
+{
+}
--- Server/TheGame/GameService.h
+++ Server/TheGame/GameService.h
@@ -13,6 +13,7 @@ using UserId = int;
 
 class GameService
 {
+public:
 	GameService();
 
 	std::optional<GameId> FindGame(UserId userId, const std::string& username, int desiredPlayerCount);
@@ -20,5 +21,24 @@ class GameService
 	std::optional<GameId> GetPlayerGameStatus(UserId userId);
 
 	Game& GetGame(const GameId gameId);
+private:
+	std::mutex m_mutex;
+
+	struct WaitingPlayer
+	{
+		UserId id;
+		std::string name;
+		std::chrono::steady_clock::time_point joinTime;
+	};
+
+	std::map<int, std::list<WaitingPlayer>> m_waitingQueues;
+
+	std::map<GameId, Game> m_activeGames;
+
+	std::map<UserId, int> m_playerInQueueMap;
+
+	std::map<UserId, GameId> m_playerGameMap;
+
+	void CreateGame(std::list<WaitingPlayer>& players, int playerCount);
 };
 

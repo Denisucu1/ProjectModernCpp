cfa1f5685b4f93187e95b007d3a2ed4b960943c2
Oatu Mihai <mihai.oatu@student.unitbv.ro>
2025-11-21 09:51:41 +0200

    implemented match api

--- Server/TheGame/ApiRoutes.cpp
+++ Server/TheGame/ApiRoutes.cpp
@@ -108,5 +108,55 @@ void setupRoutes(crow::SimpleApp& app, UserService& userSvc)
         res.add_header("Access-Control-Allow-Origin", "*");
         return res;
             });
+
+    CROW_ROUTE(app, "/api/match/<int>").methods("GET"_method)
+        ([/*&gameSvc*/](const crow::request& req, int matchId) {
+
+        if (matchId == 0) {
+            return crow::response(404, "{\"error\": \"Match not found (Mock)\"}");
+        }
+
+        crow::json::wvalue res;
+
+        res["matchId"] = matchId;
+        res["status"] = "InProgress";
+        res["currentTurnPlayerId"] = 101; 
+
+
+        res["stacksState"] = crow::json::load("[1, 1, 100, 100]");
+
+        crow::json::wvalue deckObj;
+        deckObj["cardsRemaining"] = 40;
+        deckObj["topDiscard"] = 0;
+        res["deckState"] = std::move(deckObj);
+
+
+        // Player Simulation
+        std::vector<MatchPlayer> players;
+        MatchPlayer p1;
+        p1.Id = 101; p1.UserId = 5; p1.CardsInHandJSON = "[10, 20, 30]"; 
+        MatchPlayer p2;
+        p2.Id = 102; p2.UserId = 8; p2.CardsInHandJSON = "[5, 15, 25]";
+        players.push_back(p1);
+        players.push_back(p2);
+
+        std::vector<crow::json::wvalue> playersJsonList;
+
+        for (const auto& player : players) {
+            crow::json::wvalue pJson;
+            pJson["id"] = player.Id;
+            pJson["userId"] = player.UserId;
+
+            auto handJson = crow::json::load(player.CardsInHandJSON);
+            if (handJson) pJson["cardsInHand"] = handJson;
+            else pJson["cardsInHand"] = crow::json::load("[]");
+
+            playersJsonList.push_back(std::move(pJson));
+        }
+
+        res["players"] = std::move(playersJsonList);
+
+        return crow::response(200, res);
+     });
 }
 

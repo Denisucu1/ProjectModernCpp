ba16ace7be00de815c6e7d58ade9896a03153dc3
aurelianv1 <auras.anca17@gmail.com>
2025-11-23 21:05:04 +0200

    added half start method

--- Server/TheGame/Game.cpp
+++ Server/TheGame/Game.cpp
@@ -2,10 +2,21 @@
 
 Game::Game(std::vector<Player> players) : m_players(players)
 {
+    m_currentPlayerIndex = 0;
+
+    // Pornim jocul imediat ce este creat
+    StartGame();
 }
 
 void Game::StartGame()
 {
+    int cardsPerPlayer = 6; // Default pentru 4-5 jucatori
+    if (m_players.size() == 2) {
+        cardsPerPlayer = 8; // Regula: 2 jucatori = 8 carti
+    }
+    else if (m_players.size() == 3) {
+        cardsPerPlayer = 7; // Regula: 3 jucatori = 7 carti
+    }
 }
 
 void Game::GameStatus()

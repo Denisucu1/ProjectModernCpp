22b83469350afbac765275715b91479c45810f26
aurelianv1 <auras.anca17@gmail.com>
2025-11-23 21:14:28 +0200

    other updates for startgame

--- Server/TheGame/Game.cpp
+++ Server/TheGame/Game.cpp
@@ -18,7 +18,20 @@ void Game::StartGame()
 		cardsPerPlayer = 7;
 	}
 
+	std::cout << "[Game] Start: " << m_players.size() << " jucatori. Carti per mana: " << cardsPerPlayer << std::endl;
 
+	for (auto& player : m_players) {
+		std::vector<std::uint8_t> currentHand;
+
+		for (int i = 0; i < cardsPerPlayer; ++i) {
+			if (!m_cards.IsEmpty()) {
+				Card drawnCard = m_cards.DrawACard();
+				currentHand.push_back(drawnCard.GetValue());
+			}
+		}
+		player.SetDeck(currentHand);
+	}
+	m_currentPlayerIndex = 0;
 }
 
 void Game::GameStatus()

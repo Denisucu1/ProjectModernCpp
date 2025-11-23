2f8ac3f91412f6629463328f96555457b11db5b9
aurelianv1 <auras.anca17@gmail.com>
2025-11-23 21:15:26 +0200

    update 2

--- Server/TheGame/Game.cpp
+++ Server/TheGame/Game.cpp
@@ -36,6 +36,23 @@ void Game::StartGame()
 
 void Game::GameStatus()
 {
+	std::cout << "\n=== STATUS JOC ===" << std::endl;
+
+	std::cout << "Gramezi Ascendente (Start 1): ["
+		<< (int)m_playPiles.GetAscendingColumn1() << "] si ["
+		<< (int)m_playPiles.GetAscendingColumn2() << "]" << std::endl;
+
+	std::cout << "Gramezi Descendente (Start 100): ["
+		<< (int)m_playPiles.GetDescendingColumn1() << "] si ["
+		<< (int)m_playPiles.GetDescendingColumn2() << "]" << std::endl;
+
+	if (!m_players.empty()) {
+		std::cout << "Randul jucatorului: " << m_currentPlayerIndex
+			<< " (Carti in mana: " << m_players[m_currentPlayerIndex].GetDeck().size() << ")" << std::endl;
+	}
+
+	std::cout << "Carti ramase in pachet: " << m_cards.GetDrawPile().size() << std::endl;
+	std::cout << "==================\n" << std::endl;
 }
 
 void Game::GameEndConditions()

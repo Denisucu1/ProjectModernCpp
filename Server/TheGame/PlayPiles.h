4bf0ae37a60c2cc5edf1708892747647bc464d1b
Denisucu1 <denisro463@gmail.com>
2025-11-29 19:12:08 +0200

    Adaugare enum

--- Server/TheGame/PlayPiles.h
+++ Server/TheGame/PlayPiles.h
@@ -1,24 +1,27 @@
 #pragma once
 #include "Card.h"
+#include <array>
+#include <cstdint>
 
 class PlayPiles
 {
 public:
 	PlayPiles();
 
-	std::uint8_t get_ascending_column1() const;
-	std::uint8_t get_ascending_column2() const;
+	enum StackIndex : size_t {
+		ascend_1 = 0,
+		ascend_2 = 1,
+		descend_1 = 2,
+		descend_2 = 3
+	};
 
-	std::uint8_t get_descending_column1() const;
-	std::uint8_t get_descending_column2() const;
+	std::uint8_t get_stack_value(StackIndex index) const;
 
 	//verificare daca pot adauga
 
-	bool is_ascend1(const Card& card) const;
-	bool is_ascend2(const Card& card) const;
+	void play_card_on_stack(StackIndex index, std::uint8_t cardValue);
 
-	bool is_descend1(const Card& card) const;
-	bool is_descend2(const Card& card) const;
+	bool is_move_valid(StackIndex index, const Card& card) const;
 
 	//adaugare carti in gramezi
 
@@ -26,13 +29,6 @@ public:
 	//trebuie sa fac aici metode pentru verificarea de carti
 
 private:
-	std::uint8_t m_ascendingColumn1 : 7, m_ascendingColumn2 : 7,
-		m_descendingColumn1 : 7, m_descendingColumn2 : 7; //sa fie de tipul carte
+	std::array<std::uint8_t, 4> m_stacks; //sa fie de tipul carte
 };
 
-/*
-	m_ascendingColumns1 = coloana 1
-	m_ascendingColumns2 = coloana 1, elemente crescatoare
-	m_descendingColums1 = coloana 100
-	m_descendingColumns2 = coloana 100, elemente descrescatoare
-*/

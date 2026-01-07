af5e0f977226eaf138506650318a636de93789e8
aurelianv1 <auras.anca17@gmail.com>
2026-01-07 20:12:20 +0200

    Added an event for custom mode

--- Server/TheGame/CustomMode.cpp
+++ Server/TheGame/CustomMode.cpp
@@ -8,3 +8,26 @@ Card CustomMode::GetNeutralPile() const
 {
 	return neutralPile;
 }
+
+std::vector<Card> CustomMode::AddedRandomCardsInDrawPile()
+{
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+
+	std::uniform_int_distribution<size_t> countDistrib(5, 13);
+	std::uniform_int_distribution<std::uint8_t> valueDistrib(2, 98);
+
+	size_t numCardsToGenerate = countDistrib(gen);
+
+	std::vector<Card> newCards;
+
+	newCards.reserve(numCardsToGenerate);
+
+	for (size_t i = 0; i < numCardsToGenerate; i++)
+	{
+		std::uint8_t randomValue = valueDistrib(gen);
+		newCards.emplace_back(randomValue);
+	}
+
+	return newCards;
+}
--- Server/TheGame/CustomMode.h
+++ Server/TheGame/CustomMode.h
@@ -1,11 +1,18 @@
 #pragma once
 #include <vector>
 #include "Card.h"
+
+#include <random>
+
 class CustomMode
 {
 public:
 
 	CustomMode();
+
+	Card GetNeutralPile() const;
+
+	std::vector<Card> AddedRandomCardsInDrawPile();
 
 private:
 	std::vector<std::uint8_t> events;

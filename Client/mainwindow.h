5ba941a0a3b8755d9ff310964fe478f6a33d311f
Paleu Anca-Nicoleta <anca.paleu@student.unitbv.ro>
2026-01-14 15:53:10 +0200

    apply clean code principles to mainwindow

--- Client/mainwindow.h
+++ Client/mainwindow.h
@@ -3,10 +3,11 @@
 
 #include <QMainWindow>
 #include <QNetworkAccessManager>
+#include <QNetworkReply>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 QT_END_NAMESPACE
 
@@ -15,18 +16,25 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    MainWindow(QWidget* parent = nullptr);
+    ~MainWindow() override;
 
 private slots:
-
     void on_registerButton_clicked();
     void on_loginButton_clicked();
-    void onLoginReply(QNetworkReply *reply);
-    void onRegisterReply(QNetworkReply *reply);
+    void onLoginReply(QNetworkReply* reply);
+    void onRegisterReply(QNetworkReply* reply);
 
 private:
-    Ui::MainWindow *ui;
-    QNetworkAccessManager *m_networkManager;
+    void setupUiElements();
+    void applyStyles();
+    void executeAuthRequest(const QString& url);
+
+    Ui::MainWindow* ui;
+    QNetworkAccessManager* m_networkManager;
+
+    const QString apiLoginUrl = "http://localhost:18080/api/login";
+    const QString apiRegisterUrl = "http://localhost:18080/api/register";
+    const QString logoPath = ":/LOGO.png";
 };
 #endif

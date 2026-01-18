813870a4aad9c96808140710613ff1b927bd5fd5
Paleu Anca-Nicoleta <anca.paleu@student.unitbv.ro>
2026-01-18 21:43:46 +0200

    fixed magic numbers in profile widget

--- Client/profilewidget.cpp
+++ Client/profilewidget.cpp
@@ -1,7 +1,6 @@
 ﻿#include "profilewidget.h"
 #include <QVBoxLayout>
 #include <QGridLayout>
-#include <QFrame>
 
 ProfileWidget::ProfileWidget(const QString& username, QWidget* parent) : QWidget(parent) {
     setupLayout(username);
@@ -13,31 +12,33 @@ void ProfileWidget::setupLayout(const QString& username) {
 
     QWidget* profileCard = new QWidget();
     profileCard->setObjectName("profileCard");
-    profileCard->setFixedWidth(400);
+    profileCard->setFixedWidth(m_cardFixedWidth);
 
     QVBoxLayout* cardLayout = new QVBoxLayout(profileCard);
-    cardLayout->setContentsMargins(30, 30, 30, 30);
+    cardLayout->setContentsMargins(m_cardMargins, m_cardMargins, m_cardMargins, m_cardMargins);
 
     QLabel* nameHeader = new QLabel(username.toUpper());
-    nameHeader->setStyleSheet("font-size: 24px; font-weight: bold; color: #4CAF50;");
+    nameHeader->setObjectName("profileNameHeader");
     nameHeader->setAlignment(Qt::AlignCenter);
     cardLayout->addWidget(nameHeader);
 
     QGridLayout* statsGrid = new QGridLayout();
     auto addStatRow = [&](const QString& text, QLabel*& label, int row) {
         QLabel* title = new QLabel(text);
-        title->setStyleSheet("color: #AAAAAA; font-size: 14px;");
-        label = new QLabel("0");
-        label->setStyleSheet("color: white; font-size: 16px; font-weight: bold;");
+        title->setProperty("class", "statTitle");
+
+        label = new QLabel(m_defaultStatValue);
+        label->setProperty("class", "statValue");
+
         statsGrid->addWidget(title, row, 0, Qt::AlignLeft);
         statsGrid->addWidget(label, row, 1, Qt::AlignRight);
         };
 
-    addStatRow("Games Played", m_gamesPlayedLabel, 0);
-    addStatRow("Games Won", m_gamesWonLabel, 1);
-    addStatRow("Cards at Loss", m_cardsAtLossLabel, 2);
-    addStatRow("Total Time (min)", m_totalTimeLabel, 3);
-    addStatRow("Performance Score", m_performanceScoreLabel, 4);
+    addStatRow(m_labelGamesPlayed, m_gamesPlayedLabel, 0);
+    addStatRow(m_labelGamesWon, m_gamesWonLabel, 1);
+    addStatRow(m_labelCardsLoss, m_cardsAtLossLabel, 2);
+    addStatRow(m_labelTotalTime, m_totalTimeLabel, 3);
+    addStatRow(m_labelPerfScore, m_performanceScoreLabel, 4);
 
     cardLayout->addLayout(statsGrid);
     mainLayout->addWidget(profileCard, 0, Qt::AlignCenter);
@@ -45,9 +46,10 @@ void ProfileWidget::setupLayout(const QString& username) {
 }
 
 void ProfileWidget::updateStats(const QJsonObject& json) {
-    m_gamesPlayedLabel->setText(QString::number(json["gamesPlayed"].toInt()));
-    m_gamesWonLabel->setText(QString::number(json["gamesWon"].toInt()));
-    m_totalTimeLabel->setText(QString::number(json["hoursPlayed"].toInt()));
-    m_cardsAtLossLabel->setText(QString::number(json["cardsLeftOnLosses"].toInt()));
-    m_performanceScoreLabel->setText(QString::number(json["performanceScore"].toDouble(), 'f', 2));
+    m_gamesPlayedLabel->setText(QString::number(json[m_keyGamesPlayed].toInt()));
+    m_gamesWonLabel->setText(QString::number(json[m_keyGamesWon].toInt()));
+    m_totalTimeLabel->setText(QString::number(json[m_keyHoursPlayed].toInt()));
+    m_cardsAtLossLabel->setText(QString::number(json[m_keyCardsLeft].toInt()));
+
+    m_performanceScoreLabel->setText(QString::number(json[m_keyPerfScore].toDouble(), 'f', m_precision));
 }
--- Client/profilewidget.h
+++ Client/profilewidget.h
@@ -9,7 +9,7 @@ class ProfileWidget : public QWidget {
     Q_OBJECT
 public:
     explicit ProfileWidget(const QString& username, QWidget* parent = nullptr);
-    void updateStats(const QJsonObject& data); 
+    void updateStats(const QJsonObject& data);
 
 private:
     void setupLayout(const QString& username);
@@ -19,6 +19,24 @@ private:
     QLabel* m_cardsAtLossLabel;
     QLabel* m_totalTimeLabel;
     QLabel* m_performanceScoreLabel;
+
+    const int m_cardFixedWidth = 400;
+    const int m_cardMargins = 30;
+    const int m_spacingLarge = 30;
+    const int m_precision = 2;
+
+    const QString m_keyGamesPlayed = "gamesPlayed";
+    const QString m_keyGamesWon = "gamesWon";
+    const QString m_keyHoursPlayed = "hoursPlayed";
+    const QString m_keyCardsLeft = "cardsLeftOnLosses";
+    const QString m_keyPerfScore = "performanceScore";
+
+    const QString m_labelGamesPlayed = "Games Played";
+    const QString m_labelGamesWon = "Games Won";
+    const QString m_labelCardsLoss = "Cards at Loss";
+    const QString m_labelTotalTime = "Total Time (min)";
+    const QString m_labelPerfScore = "Performance Score";
+    const QString m_defaultStatValue = "0";
 };
 
 #endif
